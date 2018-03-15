a726e8455ef40cf28e25619c9b33147b4e295e43
Teleportal <joshthegreenberg@gmail.com>
2018-03-15 18:44:24 -0500

    added functionality

--- lib/zero_dawn.rb
+++ lib/zero_dawn.rb
@@ -4,6 +4,7 @@ module ZeroDawn
   class Gaia
     def hades
       `say oh noooooooooooooooooooooooooooooooooooooooo kill all the humans`
+      `say to save all the humans... i guess`
     end
   end
 end
